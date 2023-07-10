0c085825ea60d34556edefa7b3333cf648e1dc60
amin <mnrzghi@gmail.com>
2023-07-10 18:03:50 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Basic vscode configs for OpenFOAM files
+
+Clone repository and start foaming
+```shell
+git clone https:...
+```
---
+++ helloworld.C
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "hello Foam!" << std::endl;
+
+    return 0;
+}
